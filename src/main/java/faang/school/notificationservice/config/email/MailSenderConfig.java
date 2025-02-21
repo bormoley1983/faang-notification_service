684ce2bb3fb6adb9000397d05680ca1fc044d992
DmitryQueen <276246@student.pwr.edu.pl>
2025-02-21 15:42:25 +0100

    fixed mailSenderConfig

--- src/main/java/faang/school/notificationservice/config/email/MailSenderConfig.java
+++ src/main/java/faang/school/notificationservice/config/email/MailSenderConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.util.Properties;
+
 @Configuration
 public class MailSenderConfig {
 
@@ -15,12 +17,26 @@ public class MailSenderConfig {
     @Value("${spring.mail.password}")
     private String password;
 
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private Integer port;
+
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
+        mailSender.setHost(host);
+        mailSender.setPort(port);
         mailSender.setUsername(email);
         mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
 
         return mailSender;
     }
