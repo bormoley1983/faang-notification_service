6d4de43bd9fe28cfd5b540c89cfedc6fa82e8217
Bormoley1983 <zhuravel_leonid@yahoo.com>
2026-09-01 22:16:03 +0300

    refactor: Remove unused Mockito imports in test classes

--- src/test/java/faang/school/notificationservice/bot/TelegramBotTest.java
+++ src/test/java/faang/school/notificationservice/bot/TelegramBotTest.java
@@ -22,7 +22,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.never;
--- src/test/java/faang/school/notificationservice/service/EmailServiceTests.java
+++ src/test/java/faang/school/notificationservice/service/EmailServiceTests.java
@@ -21,7 +21,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class EmailServiceTests {
