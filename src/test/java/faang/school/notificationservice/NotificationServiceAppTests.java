705fe5aac9c2c6e4ad6cb2ec5f760c00786554fc
Bormoley1983 <zhuravel_leonid@yahoo.com>
2026-09-01 19:01:10 +0300

    Refactor application.yaml structure and enhance NotificationServiceAppTests with additional Kafka topic assertions

--- src/test/java/faang/school/notificationservice/NotificationServiceAppTests.java
+++ src/test/java/faang/school/notificationservice/NotificationServiceAppTests.java
@@ -1,12 +1,33 @@
 package faang.school.notificationservice;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.env.YamlPropertySourceLoader;
+import org.springframework.core.env.StandardEnvironment;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 class NotificationServiceAppTests {
     @Test
-    void contextLoads() {
-        Assertions.assertThat(40 + 2).isEqualTo(42);
+    void applicationYamlDefinesKafkaConsumerPropertiesUnderSpringNamespace() throws IOException {
+        var environment = new StandardEnvironment();
+        var propertySources = new YamlPropertySourceLoader()
+                .load("application", new ClassPathResource("application.yaml"));
+        propertySources.forEach(environment.getPropertySources()::addLast);
+
+        assertThat(environment.getProperty("spring.kafka.bootstrap-servers"))
+                .isEqualTo("localhost:9092");
+        assertThat(environment.getProperty("spring.kafka.consumer.group-id"))
+                .isEqualTo("notification-group");
+        assertThat(environment.getProperty("spring.kafka.topics.comment-topic.name"))
+                .isEqualTo("notification_comment_topic");
+        assertThat(environment.getProperty("spring.kafka.topics.like-topic.name"))
+                .isEqualTo("notification_like_topic");
+        assertThat(environment.getProperty("spring.kafka.topics.event-start-topic.name"))
+                .isEqualTo("event_start_topic");
+        assertThat(environment.containsProperty("notification.kafka.bootstrap-servers")).isFalse();
     }
 }
